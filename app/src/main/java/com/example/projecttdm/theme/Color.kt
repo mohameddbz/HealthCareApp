41b6106bc0f633f2965156dbadd4ffa419a10724
Thanina Amirat <150529781+thanina-amrt@users.noreply.github.com>
2025-03-27 14:45:42 +0100

    Update des couleurs

--- app/src/main/java/com/example/projecttdm/theme/Color.kt
+++ app/src/main/java/com/example/projecttdm/theme/Color.kt
@@ -4,10 +4,7 @@ package com.example.projecttdm.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Blue01 = Color(0xFF3E69FE)
+val Gray01 = Color(0xFF9CA3AF)
+val Gray02 = Color(0xFF1C2A3A)
+val Blue02 = Color(0xFFDEE9FB)
