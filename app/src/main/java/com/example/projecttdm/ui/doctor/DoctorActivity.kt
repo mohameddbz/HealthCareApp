bdd38a5dc104b7a275d55b6585c66cc3bfffcde3
Oussama BOUCENNA <lo_boucenna@esi.dz>
2025-05-16 19:37:00 +0100

    add doctor Navigation

--- app/src/main/java/com/example/projecttdm/ui/doctor/DoctorActivity.kt
+++ app/src/main/java/com/example/projecttdm/ui/doctor/DoctorActivity.kt
@@ -16,6 +16,7 @@ class DoctorActivity : ComponentActivity() {
         setContent {
             ProjectTDMTheme{
                // DoctorNavigation()
+                DoctorNavigation()
             }
         }
     }
