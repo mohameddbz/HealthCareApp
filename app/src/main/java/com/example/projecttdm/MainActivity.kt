bf0799b43bf6b6f487726cd3c2b8ca976b296f58
thanina-amrt <lt_amirat@esi.dz>
2025-05-18 15:51:10 +0100

    last

--- app/src/main/java/com/example/projecttdm/MainActivity.kt
+++ app/src/main/java/com/example/projecttdm/MainActivity.kt
@@ -16,84 +16,75 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.lifecycleScope
 import com.example.projecttdm.data.endpoint.ApiClient
-import com.example.projecttdm.data.local.PatientData
-import com.example.projecttdm.data.local.SampleDataProvider
-import com.example.projecttdm.data.model.Patient
-import com.example.projecttdm.data.repository.DoctorRepository
 import com.example.projecttdm.theme.ProjectTDMTheme
 import com.example.projecttdm.ui.auth.AuthActivity
 import com.example.projecttdm.ui.auth.AuthNavigation
 import com.example.projecttdm.ui.doctor.DoctorActivity
 import com.example.projecttdm.ui.doctor.DoctorNavigation
-import com.example.projecttdm.ui.doctor.screens.AppointmentOfWeekScreen
 import com.example.projecttdm.ui.patient.PatientActivity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.time.LocalDate
-
-
 
 
 class MainActivity : ComponentActivity() {
-
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge() // Enable edge-to-edge display
 
-        
-
+        // Show a splash screen while checking authentication
         setContent {
             ProjectTDMTheme {
-                DoctorNavigation(
-                )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Box(contentAlignment = Alignment.Center) {
+                        CircularProgressIndicator()
+                    }
+                }
+            }
+        }
+
+        // Check authentication status in a background thread
+        lifecycleScope.launch(Dispatchers.IO) {
+            // Get authentication data from SharedPreferences
+            val sharedPreferences = getSharedPreferences("doctor_prefs", MODE_PRIVATE)
+            val token = sharedPreferences.getString("auth_token", null)
+            val userConnected = sharedPreferences.getBoolean("user_connected", false)
+            val role = sharedPreferences.getString("role",null)
+
+
+            // Switch back to main thread to navigate
+            withContext(Dispatchers.Main) {
+                // Navigate to appropriate screen based on authentication status
+                if (token.isNullOrEmpty() || !userConnected) {
+                    navigateToAuth()
+                } else {
+                    ApiClient.setTokenProvider { token }
+                    if(role =="doctor") {
+                        navigateToDoctor()
+                    }else if (role == "patient") {
+                        navigateToPatient()
+                    }
+
+                }
             }
         }
     }
+
+    private fun navigateToAuth() {
+        val intent = Intent(this, AuthActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+    private fun navigateToDoctor(){
+        val intent = Intent(this,DoctorActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun navigateToPatient() {
+        val intent = Intent(this, PatientActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 }
-
-
-//        // Check authentication status in a background thread
-//        lifecycleScope.launch(Dispatchers.IO) {
-//            // Get authentication data from SharedPreferences
-//            val sharedPreferences = getSharedPreferences("doctor_prefs", MODE_PRIVATE)
-//            val token = sharedPreferences.getString("auth_token", null)
-//            val userConnected = sharedPreferences.getBoolean("user_connected", false)
-//            val role = sharedPreferences.getString("role",null)
-//
-//
-//            // Switch back to main thread to navigate
-//            withContext(Dispatchers.Main) {
-//                // Navigate to appropriate screen based on authentication status
-//                if (token.isNullOrEmpty() || !userConnected) {
-//                    navigateToAuth()
-//                } else {
-//                    ApiClient.setTokenProvider { token }
-//                    if(role =="doctor") {
-//                        navigateToDoctor()
-//                    }else if (role == "patient") {
-//                        navigateToPatient()
-//                    }
-//
-//                }
-//            }
-//        }
-//    }
-//
-//    private fun navigateToAuth() {
-//        val intent = Intent(this, AuthActivity::class.java)
-//        startActivity(intent)
-//        finish()
-//    }
-//    private fun navigateToDoctor(){
-//        val intent = Intent(this,DoctorActivity::class.java)
-//        startActivity(intent)
-//        finish()
-//    }
-//
-//    private fun navigateToPatient() {
-//        val intent = Intent(this, PatientActivity::class.java)
-//        startActivity(intent)
-//        finish()
-//    }
-//}
